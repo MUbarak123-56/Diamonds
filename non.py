05df63251282fd35a584d9fda7d85b0e80f761a0
TheDataMan <41446936+MUbarak123-56@users.noreply.github.com>
2021-01-30 19:36:35 -0600

    new commit

---
+++ non.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib as plt
+import seaborn as sns
+import streamlit as st
+import sklearn as sk 
+print(sk.__version__)
+print(sns.__version__)
+print(pd.__version__)
+print(np.__version__)
+print(plt.__version__)
+print(st.__version__)
